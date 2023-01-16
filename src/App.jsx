ef9327208d11a977922e92251aa35d4d32bfea3d
Wagner Guedes <97996373+wagner-guedes-dev@users.noreply.github.com>
2023-01-15 22:24:37 -0300

    challenge concluded

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,78 @@
+// # React Interview Challenge.
+
+// Desenvolva uma aplicação em que:
+
+// - o usuário pode clicar em qualquer lugar da página.
+// - deve-se renderizar um pequeno círculo na posição clicada.
+// - a cada clique, mantém-se os círculos já criados e renderiza-se um novo.
+// - crie duas funcionalidades para a aplicação:
+//     - desfazer (undo)
+//     - refazer (redo)
+
+
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+  const [undid, setUndid] = useState([]);
+
+  const handlePoint = (e) =>{
+    console.log(e)
+    const positions = {
+      eixoX: e.clientX,
+      eixoY: e.clientY
+    }
+    setList((prev)=> [...prev, positions])
+    setUndid([])
+  }
+  
+  const handleUndo = (e)=>{
+    e.stopPropagation()
+
+    if(list.length === 0){
+      return
+    }
+    const lastItem = list[list.length - 1];
+    setUndid((prev) => [...prev, lastItem]);
+
+    setList((prev)=>{
+      const newArr = [...prev].slice(0, -1)
+      return newArr
+    })
+  }
+
+
+  
+  const handleRedo = (e)=>{
+    e.stopPropagation()
+
+    if (undid.length === 0) {
+      return;
+    }
+
+    const recoveredDot = undid[undid.length - 1];
+    setUndid((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+    setList((prev) => [...prev, recoveredDot]);
+
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" onClick={handlePoint}>
+
+      <div className='buttons'>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleRedo}>Redo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {list.map((e, index)=>(
+        <span key={index} className='point' style={{top:e.eixoY, left:e.eixoX}}/>
+      ))}
     </div>
   )
 }
